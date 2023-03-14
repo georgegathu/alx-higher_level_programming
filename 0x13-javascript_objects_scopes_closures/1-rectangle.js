b429a02ada39b05e505da20cfecf02934773ea6b
georgegathu <ggeorge.gathu@gmail.com>
2023-03-14 17:18:57 +0300

    a class Rectangle that defines a rectangle correction

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 /**
- * Check the parameters provided
+ * Represents a rectangle class with width and height attributes
  */
 class Rectangle {
   constructor (w, h) {
-    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
+    this.width = w;
+    this.height = h;
   }
 }
 module.exports = Rectangle;
