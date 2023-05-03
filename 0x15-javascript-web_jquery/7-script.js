78b49aa6c53cce4f696ae64aaee5a9a8ded9db07
georgegathu <ggeorge.gathu@gmail.com>
2023-05-03 22:14:48 +0300

    7. Star wars character

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,11 @@
+$(function () {
+  const character = $('div#character');
+
+  $.ajax({
+    url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+    dataType: 'json',
+    success: function (data) {
+      character.text(data.name);
+    }
+  });
+});
